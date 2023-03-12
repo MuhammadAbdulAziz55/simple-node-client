328c35585a9225fef8dfe98186240928bb5357f3
MuhammadAbdulAziz55 <abdulazizfrince5@gmail.com>
2023-03-12 10:08:39 +0600

    It's backend module first project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleUsers = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const newUser = [...users, data];
+        setUsers(newUser);
+      })
+      .catch((err) => console.error(err));
+    console.log(user);
+
+    event.target.reset();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Total Users: {users.length}</h1>
+
+      <form onSubmit={handleUsers}>
+        <input name="name" type="text" placeholder="name" />
+        <br />
+        <input name="email" type="email" placeholder="email" />
+        <br />
+        <button type="submit">Add User</button>
+      </form>
+      {users.map((user) => (
+        <p key={user._id}>{user.email}</p>
+      ))}
     </div>
   );
 }
